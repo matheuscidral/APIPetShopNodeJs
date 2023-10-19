d11dee2a4dd0225ee195ed5123d40f581bdaf662
matheuscidral <matheus.costa.cidral@gmail.com>
2023-10-19 19:49:40 -0300

    Começando implementação do sistema de autenticação no petshop

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const router = require("./src/routes/atendimento.js");
+const routers = require("./src/routes/pessoa.js");
 
 const app = express();
-const port = 3000;
 
 app.use(express.json());
-app.use(router);
 
-app.listen(port, () => {
-  console.log(`>>> Servidor rodando na porta ${port} <<<`);
+app.use(routers);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
